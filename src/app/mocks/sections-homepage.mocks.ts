25081e688ee8d2e1963be521efd9bc70667d657a
Clara Charriau <101406252+ClaraCharriau@users.noreply.github.com>
2022-12-09 21:12:01 +0100

    [feat/homepage-section] Creation des sections de la homepage (#6)
    * [feat/homepage-section] Creation des sections de la homepage

---
+++ src/app/mocks/sections-homepage.mocks.ts
@@ -0,0 +1,31 @@
+export interface SectionHomepage {
+    title: string;
+    paragraph: string;
+    button: string;
+    illustration: string;
+    illustrationAlt: string;
+}
+
+export const SECTIONHOMEPAGE: SectionHomepage[] = [
+    {
+        title: "Plantes d'intérieur",
+        paragraph: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus ab consequuntur cumque delectus? Facilis blanditiis dolor, incidunt ipsum eos placeat laudantium excepturi quis vero. Nesciunt optio blanditiis rem ullam tempora?',
+        button: 'En savoir plus',
+        illustration: 'assets/img/homepage-photos/photo-plante-interieur.png',
+        illustrationAlt: "grande plante d'intérieur"
+    },
+    {
+        title: "Fleur séchées",
+        paragraph: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus ab consequuntur cumque delectus? Facilis blanditiis dolor, incidunt ipsum eos placeat laudantium excepturi quis vero. Nesciunt optio blanditiis rem ullam tempora?',
+        button: 'En savoir plus',
+        illustration: 'assets/img/homepage-photos/fleurs-sechees.png',
+        illustrationAlt: ''
+    },
+    {
+        title: "Bouquets de fleurs",
+        paragraph: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus ab consequuntur cumque delectus? Facilis blanditiis dolor, incidunt ipsum eos placeat laudantium excepturi quis vero. Nesciunt optio blanditiis rem ullam tempora?',
+        button: 'En savoir plus',
+        illustration: 'assets/img/homepage-photos/colorful-bouquet.png',
+        illustrationAlt: ''
+    }
+]
