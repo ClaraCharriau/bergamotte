7412747ff4f542d625cf5ed27b5bb262702e82df
Clara Charriau <charriau.clara@gmail.com>
2022-12-09 16:29:36 +0100

    [feat/landing-homepage] create component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ProductCardsComponent } from './components/product-cards/product-cards.component';
 import { ProductCategoryComponent } from './components/product-category/product-category.component';
+import { LandingWelcomeComponent } from './components/landing-welcome/landing-welcome.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { ProductCategoryComponent } from './components/product-category/product-
     FooterComponent,
     ProductCardsComponent,
     ProductCategoryComponent,
+    LandingWelcomeComponent,
   ],
   imports: [
     BrowserModule
